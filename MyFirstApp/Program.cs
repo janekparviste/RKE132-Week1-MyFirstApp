ac9435620e77f31f80247d77614be283c0b70057
JanekRoos <janek.roos@yandex.ru>
2024-01-30 19:18:58 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//output ehk väljund
+//string - sõne
+string userName = Console.ReadLine();//input ehk sisend
+
+Console.WriteLine("Hello, " + userName + " !");//output ehk väljund
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");//output ehk väljund
